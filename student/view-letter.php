7e7b6e514937c870c9d27df1673fb48eda7fb673
duraisankar <rduraisankar@gmail.com>
2018-03-08 22:26:06 +0530

    Inbox added for Student

---
+++ student/view-letter.php
@@ -0,0 +1,302 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Eco Letter| Dashboard</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="../bower_components/Ionicons/css/ionicons.min.css">
+    <!-- jvectormap -->
+    <link rel="stylesheet" href="../bower_components/jvectormap/jquery-jvectormap.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
+
+    <link rel="stylesheet" href="../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Google Font -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition skin-red sidebar-mini fixed">
+<div class="wrapper">
+
+    <header class="main-header">
+
+        <!-- Logo -->
+        <a href="index2.html" class="logo">
+            <!-- mini logo for sidebar mini 50x50 pixels -->
+            <span class="logo-mini"><b>E</b>LT</span>
+            <!-- logo for regular state and mobile devices -->
+            <span class="logo-lg"><b>Eco</b>Letter</span>
+        </a>
+
+        <!-- Header Navbar: style can be found in header.less -->
+        <nav class="navbar navbar-static-top">
+            <!-- Sidebar toggle button-->
+            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                <span class="sr-only">Toggle navigation</span>
+            </a>
+            <!-- Navbar Right Menu -->
+            <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+
+                    <li>
+                        <a href="/logout.php"><i class="fa fa-sign-out"></i> Log out</a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+    </header>
+    <!-- Left side column. contains the logo and sidebar -->
+    <aside class="main-sidebar"> <section class="sidebar">
+            <!-- sidebar: style can be found in sidebar.less -->
+            <div class="user-panel">                <div class="pull-left image">                    <img src="../dist/img/student.png" class="img-circle" alt="User Image">                </div>                <div class="pull-left info">                    <p>Student Name</p>                    <a href="#"><i class="fa fa-circle text-success"></i> Student</a>                </div>            </div>
+            <!-- Sidebar user panel -->
+
+            <!-- search form -->
+
+            <!-- /.search form -->
+            <!-- sidebar menu: : style can be found in sidebar.less -->
+            <ul class="sidebar-menu" data-widget="tree">
+                <li class="header">MAIN NAVIGATION</li>
+
+
+                <li><a href="index.php"><i class="fa fa-pie-chart"></i><span>Dashboard</span></a></li>
+                <li><a href="new-letter.php"><i class="fa fa-plus-square"></i> <span>New Letter</span></a></li>
+                <li><a href="past-letter.php"><i class="fa fa-tasks"></i> <span>Manage Letters</span></a></li>
+                <li class="active"><a href="notifications.php"><i class="fa fa-bell"></i><span>Notifications</span><span class="pull-right-container">
+              <span class="label label-primary pull-right">4</span>
+            </span></a></li>
+                <li><a href="profile.php"><i class="fa fa-user-circle"></i> <span>Profile</span></a></li>
+
+
+
+            </ul>
+        </section>
+        <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                View Letter
+                <small>Read Or Approve Letter</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">Notification</li>
+            </ol>
+        </section>
+
+        <!-- Main content -->
+
+
+
+            <section class="container">
+
+                <div class="row" style="padding: 10px;">
+                    <div class="col-md-6" style="background: #fff;padding: 10px;">
+
+                        <!-- title row -->
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <h2 class="page-header">
+                                    <i class="fa fa-envelope-open text-blue"></i> Leave Letter.
+                                    <small class="pull-right">Applied Date: 2/10/2014</small>
+                                </h2>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- info row -->
+                        <div class="row invoice-info">
+                            <div class="col-sm-4 invoice-col">
+                                From
+                                <address>
+                                    <strong>Farija Parveen.</strong><br>
+                                    4 CSE A1<br>
+                                    B.E Computer Science <br>
+
+                                </address>
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm-4 invoice-col">
+                                To
+                                <address>
+                                    <strong>Princal</strong><br>
+                                    Saveetha Engineering College<br>
+
+                                </address>
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm-4 invoice-col">
+                                <b>Letter Id #007612</b><br>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+
+
+                            <div class="row text-muted well well-sm no-shadow">
+                            <div class="col-md-6">
+                                <b><i class="fa fa-calendar-check-o"></i> Subject : Requesting Leave for hos </b>
+
+
+
+                            </div>
+
+                            <div class="col-md-6">
+
+                                <b><i class="fa fa-calendar-check-o"></i> Date : 25/12/2018 </b>
+
+                            </div>
+
+                        </div>
+
+
+
+                        <p style="font-size: large;">
+                        As I am suffering from fever, I cant able attend the class please excuse my absence
+                        </p>
+
+                        <!-- this row will not appear when printing -->
+                        <div class="row no-print">
+                            <div class="col-xs-12">
+                                <a href="" target="_blank" class="btn btn-default"><i class="fa fa-backward"></i> Back</a>
+                                <button type="button" class="btn btn-success pull-right"><i class="fa fa-check"></i> Approve Letter
+                                </button>
+                                <button type="button" class="btn btn-danger pull-right" style="margin-right: 5px;">
+                                    <i class="fa fa-ban"></i> Reject Letter
+                                </button>
+                            </div>
+                        </div>
+
+                        <HR>
+                        <div class="box-footer">
+                                <img class="img-responsive img-circle img-sm" src="../dist/img/faculty.png" alt="Alt Text">
+                                <!-- .img-push is used to add margin to elements next to floating images -->
+                                <div class="img-push">
+                                    <textarea class="form-control" placeholder="Enter comment"></textarea>
+                                </div>
+                        </div>
+
+
+                    </div>
+
+
+                    <div class="col-md-6">
+
+                        <div class="box-footer box-comments">
+                            <div class="box-comment">
+
+                                <!-- User image -->
+                                <img class="img-circle img-sm" src="../dist/img/faculty.png" alt="User Image">
+
+                                <div class="comment-text">
+                      <span class="username">
+                        Reethesh <span class="label label-success">Approved</span>
+                        <span class="text-muted pull-right">8:03 PM Today</span>
+                      </span><!-- /.username -->
+                                    It is a long established fact that a reader will be distracted
+                                    by the readable content of a page when looking at its layout.
+                                </div>
+                                <!-- /.comment-text -->
+                            </div>
+
+                            <div class="box-comment">
+                                <!-- User image -->
+                                <img class="img-circle img-sm" src="../dist/img/faculty.png" alt="User Image">
+
+                                <div class="comment-text">
+                      <span class="username">
+                        Dr. Godfrey wingston <span class="label label-warning">Pending</span>
+                        <span class="text-muted pull-right">8:03 PM Today</span>
+                      </span><!-- /.username -->
+                                    It is a long established fact that a reader will be distracted
+                                    by the readable content of a page when looking at its layout.
+                                </div>
+                                <!-- /.comment-text -->
+                            </div>
+                            <!-- /.box-comment -->
+
+                    </div>
+
+
+
+                </div>
+
+
+            </section>
+
+
+
+
+
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+            <b>Version</b> 2.4.0
+        </div>
+        <strong>Copyright &copy;  Eco Letter.</strong> All rights
+        reserved.
+    </footer>
+
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="../bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="../bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../dist/js/adminlte.min.js"></script>
+<!-- Sparkline -->
+<script src="../bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<!-- jvectormap  -->
+<script src="../plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="../plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<!-- SlimScroll -->
+<script src="../bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- ChartJS -->
+<script src="../bower_components/chart.js/Chart.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="../dist/js/pages/dashboard2.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../dist/js/demo.js"></script>
+<script>
+    $(function () {
+        //Add text editor
+        $("#compose-textarea").wysihtml5();
+    });
+</script>
+
+<script src="../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+
+</body>
+</html>
