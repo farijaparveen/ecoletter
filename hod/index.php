cec1ef618938e5e7a289897cc590e70debe7e109
Brindha.N <brindhanarayanan97@gmail.com>
2018-03-29 12:29:03 +0530

    letter content updated

--- hod/index.php
+++ hod/index.php
@@ -166,9 +166,7 @@ role_check($_SESSION['role'],4);
                       <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain
                         was born and I will give you a complete account of the system, and expound the actual teachings
                         of the great explorer of the truth, the master-builder of human happiness. No one rejects,
-                        dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know
-                        how to pursue pleasure rationally encounte fault with a man who
-                        chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that
+                        dislikes, onsequences, or one who avoids a pain that
                         produces no resultant pleasure? On the other hand, we denounce with righteous indignation and
                         dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so
                         blinded by desire, that they cannot foresee</p>
